27d992a0fc1a8d9ab740ee180ec9cc163a51f820
AirinNila <airinakhternila>
2025-05-29 20:57:58 +0600

    add some components

--- src/components/statistics/Statictics.jsx
+++ src/components/statistics/Statictics.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+
+import Banner from '../banner/Banner';
+import Categories from '../categories/Categories';
+import Featured from '../featured/Featured';
 
 const Statictics = () => {
     return (
         <div>
-            
+            <Banner></Banner>
+            <Categories></Categories>
+            <Featured></Featured>
         </div>
     );
 };
