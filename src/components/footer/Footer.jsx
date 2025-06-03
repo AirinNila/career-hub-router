de97202b468b6b786378495d9fab5a5d30a22167
AirinNila <airinakhternila>
2025-06-03 16:20:32 +0600

    footer fixed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
     return (
-        <div className=''>
+        <div className='mt-10'>
             <div><footer className="footer  sm:footer-horizontal bg-base-200 text-base-content p-10">
   <nav>
     <h6 className="footer-title">Services</h6>
